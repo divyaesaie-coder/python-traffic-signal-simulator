dee691081434bb35301cf4abdb6662f77c9a994a
E SAI RANGA NAGA DIVYA <divya.e.sai.e@gmail.com>
2026-05-23 14:44:29 +0530

    Add files via upload

---
+++ python-traffic-signal-simulator.py
@@ -0,0 +1,10 @@
+#Traffic signal simulator
+#Show Red → Yellow → Green based on time intervals using loops or timers.
+while True:
+    import time
+    print("RED")
+    time.sleep(5)
+    print("GREEN")
+    time.sleep(5)
+    print("YELLOW")
+    time.sleep(5)
